73281f4176eb784232e3a180653ae349742dab9e
Ryan-Afzal Robotics-Computer <s-afzalr@bsd405.org>
2022-01-24 17:46:11 -0800

    Remove driver control motor speed

--- src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
@@ -12,8 +12,7 @@ import frc.robot.Constants;
 public class IntakeSubsystem extends SubsystemBase {
     private final IMotorController m_intakeMotor = new VictorSPX(Constants.INTAKE_MOTOR);
 
-    private IntakeDirection m_intakeDirection = IntakeDirection.STOP;
-    private float m_motorSpeed = 1;
+    private final float m_motorSpeed = 1;
 
     private final DoubleSolenoid m_leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.INTAKE_LEFT_DEPLOY, Constants.INTAKE_LEFT_RETRACT);
     private final DoubleSolenoid m_rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.INTAKE_RIGHT_DEPLOY, Constants.INTAKE_RIGHT_RETRACT);
@@ -32,37 +31,16 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public void startIntake() {
         this.m_intakeMotor.set(ControlMode.PercentOutput, m_motorSpeed);
-        this.m_intakeDirection = IntakeDirection.FORWARD;
     }
 
     public void reverseIntake() {
         this.m_intakeMotor.set(ControlMode.PercentOutput, -m_motorSpeed);
-        this.m_intakeDirection = IntakeDirection.REVERSE;
     }
 
     public void stopIntake() {
         this.m_intakeMotor.set(ControlMode.PercentOutput, 0.0);
-        this.m_intakeDirection = IntakeDirection.STOP;
     }
     
-    public void setMotorSpeed(float speed) {
-        this.m_motorSpeed = speed;
-
-        switch (this.m_intakeDirection) {
-            case FORWARD:
-                this.startIntake();
-                break;
-            case REVERSE:
-                this.reverseIntake();
-                break;
-            case STOP:
-                this.stopIntake();
-                break;
-            default:
-                break;
-        }
-    }
-
     public void toggleArm() {
         this.m_isArmRaised = !this.m_isArmRaised;
 
@@ -74,11 +52,5 @@ public class IntakeSubsystem extends SubsystemBase {
 
         this.m_leftSolenoid.set(direction);
         this.m_rightSolenoid.set(direction);
-    }
-
-    public enum IntakeDirection {
-        FORWARD,
-        REVERSE,
-        STOP
     }
 }
