ee8fe52d3a4db2d7324c5500c630f437765f1d1b
Ryan-Afzal Robotics-Computer <s-afzalr@bsd405.org>
2022-01-24 17:04:21 -0800

    add framework for rest of subsystems

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,9 @@
 package frc.robot;
 
 public class Constants {
-
+    /*
+     * Input
+     */
 	public static final int JOYSTICK_1 = 0;
 	public static final int JOYSTICK_2 = 1;
 	public static final int JOYSTICK_3 = 2;
@@ -9,20 +11,53 @@ public class Constants {
 	public static final int QUICKTURN_BUTTON = 1;
 	public static final int JOYSTICK_2_INTAKE_BUTTON = 1;
 
-    public static final int LEFT_FRONT_ID = 1;
-    public static final int LEFT_BACK_ID = 2;
-    public static final int RIGHT_FRONT_ID = 3;
-    public static final int RIGHT_BACK_ID = 4;
-
-    // Next 2 CAN ids for 2 test motors
-    // Test CAN id for the NEO motor
-    public static final int NEO_ID = 5;
-    // Test CAN id for the Redline motor
-    public static final int REDLINE_ID = 6;
     
+    /*
+     * Drive
+     */
+    public static final int LEFT_FRONT = 1;
+    public static final int LEFT_BACK = 2;
+    public static final int RIGHT_FRONT = 3;
+    public static final int RIGHT_BACK = 4;
+
+        
     public static final int WHEEL_SIZE = 6;
     public static final double GEAR_RATIO = 1 / 10.71;
     // Calculate the position factor by calculating the circumference of the wheel times the gear ratio
     // From https://dev.revrobotics.com/sparkmax/software-resources/migrating-ctre-to-rev#change-units-from-rotations-to-inches
     public static final double POSITION_FACTOR = Math.PI * WHEEL_SIZE * GEAR_RATIO;
+
+    
+    /*
+     * Shooter
+     */
+    public static final int SHOOTER_MOTOR = -1;
+    // Motor or piston now dunno which
+    public static final int HEAD_CONTROL_MOTOR = -1;
+    public static final int HEAD_CONTROL_PISTON = -1;
+
+    /*
+     * Climb
+     */
+    public static final int LONG_ARM_MOTOR_1 = -1;
+    public static final int LONG_ARM_MOTOR_2 = -1;
+
+    public static final int SMALL_ARM_DEPLOY = -1;
+    public static final int SMALL_ARM_RETRACT = -1;
+
+
+    /*
+     * Intake
+     */
+    public static final int INTAKE_MOTOR = -1;
+
+    public static final int INTAKE_LEFT_DEPLOY = -1;
+    public static final int INTAKE_LEFT_RETRACT = -1;
+    public static final int INTAKE_RIGHT_DEPLOY = -1;
+    public static final int INTAKE_RIGHT_RETRACT = -1;
+    /*
+     * Test motors
+     */
+    public static final int NEO_ID = 5;
+    public static final int REDLINE_ID = 6; // Test intake
 }
