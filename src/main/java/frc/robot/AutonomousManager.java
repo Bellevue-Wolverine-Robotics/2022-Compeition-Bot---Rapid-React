ba30b9487578c87fd1582f0e0ef2746d0acbee43
loukylor <loukylor@gmail.com>
2022-02-07 17:40:06 -0800

    Remove selector from auto

--- src/main/java/frc/robot/AutonomousManager.java
+++ src/main/java/frc/robot/AutonomousManager.java
@@ -1,8 +1,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Commands.ArcadeDriveDistanceCommand;
 
@@ -10,11 +8,6 @@ public class AutonomousManager {
     private RobotMap m_robotMap;
 
     private DigitalInput m_switch1 = new DigitalInput(0);
-
-    private static final String DEFAULT_AUTO = "Default";
-    private static final String CUSTOM_AUTO = "My Auto";
-    private String m_autoSelected;
-    private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
     private Command m_currentCommand;
     private boolean m_isRunningCommand = false;
@@ -24,41 +17,26 @@ public class AutonomousManager {
 
     public AutonomousManager(RobotMap robotMap) {
         this.m_robotMap = robotMap;
-
-        m_chooser.setDefaultOption("Default Auto", DEFAULT_AUTO);
-        m_chooser.addOption("My Auto", CUSTOM_AUTO);
-        SmartDashboard.putData("Auto choices", m_chooser);
     }
 
     public void autonomousInit() {
-        this.m_autoSelected = m_chooser.getSelected();
-        // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-
         this.step1Command = new ArcadeDriveDistanceCommand(this.m_robotMap.getDriveTrain(), 18, 0.3);
     }
 
     public void autonomousPeriodic() {
-        switch (m_autoSelected) {
-            // Ignoring this case for now
-            //case CUSTOM_AUTO:
-            //    break;
-            case DEFAULT_AUTO:
-            default:
-                switch (this.m_step) {
-                    case 1:
-                        doStep(step1Command);
-                        break;
-                    case 2: 
-                        if (m_switch1.get()) {
-                            System.out.println("Switch is on");
-                        } else {
-                            System.out.println("Switch is off");
-                        }
-                        break;
-                    default:
-                        System.out.println("Unknown Autonomous Step");
-                        break;
+        switch (this.m_step) {
+            case 1:
+                doStep(step1Command);
+                break;
+            case 2: 
+                if (m_switch1.get()) {
+                    System.out.println("Switch is on");
+                } else {
+                    System.out.println("Switch is off");
                 }
+                break;
+            default:
+                System.out.println("Unknown Autonomous Step");
                 break;
         }
   
